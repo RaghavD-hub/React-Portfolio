c0e814947cb302197f19f21bdd01ecbdba389b89
Raghav Dobriyal <dobriyalraghav10@gmail.com>
2025-07-23 02:17:42 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import styles from './Header.module.css'
+
+// This is a placeholder for the Header component
+// You can add your header content here, such as a logo, navigation links, etc.
+
+function Header() {
+  return (
+   <Header className={StyleSheet.header}>Header</Header> 
+  )
+}
+
+export default Header
