7ace113ee2a08ec9e9a2d03cf037f957c0e42274
Raghav Dobriyal <dobriyalraghav10@gmail.com>
2025-07-24 14:42:14 +0530

    almost there

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import styles from './footer.module.css'
 
-function Footer() {
+export default function Footer() {
   return (
-    <footer id='footer'>
-      
-    </footer>
+    <div className={styles.footerWrapper}>
+      <a href="https://github.com/RaghavD-hub" rel="noreferrer" target="_blank" className={styles.icon}>
+        <i className="fa-brands fa-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/raghav-dobriyal-8a996422a" rel="noreferrer" target="_blank" className={styles.icon}>
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
+      <a href="https://www.google.com/" rel="noreferrer" target="_blank" className={styles.icon}>
+        <i className="fa-brands fa-codepen"></i>
+      </a>
+    </div>
   )
 }
-
-export default Footer
